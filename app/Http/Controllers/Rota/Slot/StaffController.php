4730f951719aeb7c3dd744fa2181d8aea80df1fc
Donovan <me.bigd@gmail.com>
2017-10-29 10:42:12 +0000

    SlotRota passed to view

--- app/Http/Controllers/Rota/Slot/StaffController.php
+++ app/Http/Controllers/Rota/Slot/StaffController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers\Rota\Slot;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Rota\Slot\Staff as SlotStaff;
+use App\Models\Rota\Slot\Staff as SlotRotaStaff;
 
 class StaffController extends Controller
 {
     //
     protected function get() {
-        $data['staff'] = SlotStaff::active()->get();
+        $data['staffRota'] = SlotRotaStaff::active()->get();
+
+        foreach($data['staffRota'] as $staffRota) {
+            $data['staffRotaBreakdown'][$staffRota->staffid][$staffRota->daynumber]['starttime'] = $staffRota->starttime;
+            $data['staffRotaBreakdown'][$staffRota->staffid][$staffRota->daynumber]['endtime'] = $staffRota->endtime;
+        }
+        print_r($data['staffRotaBreakdown']);
         return view('slot_rota', $data);
     }
 }
