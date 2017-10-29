04da33648bf2b046f30aa063e887e601f16fde93
Donovan <me.bigd@gmail.com>
2017-10-29 09:00:20 +0000

    Added Rota Slot Staff controller & Basic Route

---
+++ app/Http/Controllers/Rota/Slot/StaffController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Rota\Slot;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class StaffController extends Controller
+{
+    //
+    protected function get() {
+        return view('welcome');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Rota\Slot\StaffController@get');
