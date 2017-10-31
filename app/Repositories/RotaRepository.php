4aad28f36e91e375b9c75c84d58acc06cd0fde8a
Donovan <me.bigd@gmail.com>
2017-10-31 22:21:46 +0000

    Moved data manipulation to repository

--- app/Repositories/RotaRepository.php
+++ app/Repositories/RotaRepository.php
@@ -25,6 +25,31 @@ class RotaRepository implements RotaInterface
     
     public function getActive() 
     {
-        return $this->model->all();           
+        //Grab all rota entries that have staffids
+        $rota = $this->model->active()->orderBy('daynumber')->get();
+        $data['rotaDays'] = array();
+        $data['rotaHours'] = array();
+
+        foreach($rota as $shift) {
+            
+            //Correlate unique days
+            if(array_search($shift->daynumber, $data['rotaDays']) === false){
+                $data['rotaDays'][] = $shift->daynumber;
+            }
+            
+            //Build new array to pass to view
+            $data['rota'][$shift->staffid][$shift->daynumber]['dayoff'] = $shift->slottype == 'dayoff' ? true : 0;
+            $data['rota'][$shift->staffid][$shift->daynumber]['starttime'] = $shift->starttime;
+            $data['rota'][$shift->staffid][$shift->daynumber]['endtime'] = $shift->endtime;
+
+            $data['rotaHours'][$shift->daynumber][] = $shift->workhours;
+        }
+
+        //Clear excess data + Calculate Total Hours
+        foreach($data['rotaHours'] as $day => $hours) {
+            unset($data['rotaHours'][$day]);
+            $data['rotaHours'][$day]['total'] = array_sum($hours);
+        }
+        return $data;        
     }
 }
